f79d85cf9e405ab94b1d19856f7c865314c8141c
Jaya Kamali <jayakamali29@gmail.com>
2026-02-20 21:04:42 +0530

    Removed unwanted files

--- src/Traditional.js
+++
@@ -1,7 +0,0 @@
-const Dress = [
-  {name: "Lehenga",color: "Yellow",url: "https://i.pinimg.com/736x/c7/62/a4/c762a4d67d135d8b3f575d7bfb4a82cd.jpg"},
-  {name: "Anarkali",color: "Green",url: "https://i.pinimg.com/736x/c2/26/61/c226611dee05c6f1524251be17c7e69a.jpg"},
-  {name: "Saree",color: "Red",url: "https://i.pinimg.com/736x/20/e8/54/20e8545846e9acf270769c766392daa4.jpg"}
-];
-
-export default Dress;
--- src/kidswear.js
+++
@@ -1,7 +0,0 @@
-const Kids = [
-  {name: "Set pant",color: "Yellow",url: "https://i.pinimg.com/736x/58/b0/00/58b0003fb2fc795eb50862d367422d29.jpg"},
-  {name: "Gown", color: "Green",url: "https://www.baoleely.com/cdn/shop/files/LJ0546_1.jpg?v=1761704095"},
-  {name: "Frog", color: "Red",url: "https://www.sunnderly.com/cdn/shop/files/B9_U176_VNSI_PO_JJW76.png?v=1742958323&width=720"}
-];
-
-export default Kids;
